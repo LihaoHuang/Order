c8a30f6d80dfecbc50b35bfd4a2a31e1ef29ccdd
LihaoHuang <azxaxa@gmail.com>
2018-06-02 14:41:09 +0800

    網頁切版完成
    1. 完成切版
    2. 加入需要css / js

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,46 @@
 |
 */
 
+/*Route::group(['middleware' => ['auth']], function () {
+	Route::get('/', ['as' => 'house.index', 'uses' => 'HomeController@index']);
+	Route::get('/house/{assess_id}', ['as' => 'house.info', 'uses' => 'HouseController@info']);
+	Route::get('/house_view/{assess_id}', ['as' => 'house.view', 'uses' => 'HouseController@view']);
+	Route::get('/house_verify/{assess_id}', ['as' => 'house.verity', 'uses' => 'HouseController@verity']);
+	Route::post('/save/{assess_id}', ['as' => 'house.verity', 'uses' => 'HouseController@save']);
+});*/
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('example.index');
+});
+
+Route::get('/index', function () {
+    return view('example.index');
+});
+
+Route::get('/charts', function () {
+    return view('example.charts');
+});
+
+Route::get('/tables', function () {
+    return view('example.tables');
+});
+
+Route::get('/blank', function () {
+    return view('example.blank');
+});
+
+Route::get('/cards', function () {
+    return view('example.cards');
+});
+
+Route::get('/login', function () {
+    return view('example.login');
+});
+
+Route::get('/register', function () {
+    return view('example.register');
+});
+
+Route::get('/forgot-password', function () {
+    return view('example.forgot-password');
 });
